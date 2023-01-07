70810805bf55940dd8189d3aa936e431bc563a4b
Ezdies <bvbclubbvb@gmail.com>
2023-01-07 18:16:48 +0100

    deleted some comments

--- Crabs (bruteforce)/Crabs.cpp
+++ Crabs (bruteforce)/Crabs.cpp
@@ -5,17 +5,20 @@
 #include <map>
 #include <climits>
 
-
 using Map = std::map<int, std::vector<int>>;
 
-struct BoardContainter {
+struct BoardContainter
+{
     std::vector<std::vector<int>> &board;
     std::vector<std::vector<int>> &originalBoard;
 };
 
-bool isEmpty(std::vector<int> &result, int k) {
-    for (int i = 0; i < k; i++) {
-        if (result[i] == result[k] || abs(result[i] - result[k]) == k - i) {
+bool isEmpty(std::vector<int> &result, int k)
+{
+    for (int i = 0; i < k; i++)
+    {
+        if (result[i] == result[k] || abs(result[i] - result[k]) == k - i)
+        {
             return false;
         }
     }
@@ -23,18 +26,23 @@ bool isEmpty(std::vector<int> &result, int k) {
 }
 
 int nHetmans(std::vector<int> &result, int k, int n, std::set<std::vector<int>> &solutions,
-             std::vector<std::vector<int>> &board) {
-    if (k == n) {
-        if (solutions.find(result) == solutions.end()) {
+             std::vector<std::vector<int>> &board)
+{
+    if (k == n)
+    {
+        if (solutions.find(result) == solutions.end())
+        {
             solutions.insert(result);
         }
         return 0;
     }
 
     int minScore = INT_MAX;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         result[k] = i;
-        if (isEmpty(result, k)) {
+        if (isEmpty(result, k))
+        {
             int score = nHetmans(result, k + 1, n, solutions, board);
             minScore = std::min(minScore, score);
         }
@@ -43,93 +51,90 @@ int nHetmans(std::vector<int> &result, int k, int n, std::set<std::vector<int>> 
     return minScore;
 }
 
-
-void printBoard(std::vector<std::vector<int>> &board, const std::vector<int> &result, Map &map) {
+void printBoard(std::vector<std::vector<int>> &board, const std::vector<int> &result, Map &map)
+{
 
     int mini = INT_MAX;
     int indexMin = 0;
     int sum = 0;
     std::vector<int> indexes;
 
-    for (int i = 0; i < board.size(); i++) {
-        for (int j = 0; j < board[i].size(); j++) {
-            if (mini > board[i][result[i]]) {
+    for (int i = 0; i < board.size(); i++)
+    {
+        for (int j = 0; j < board[i].size(); j++)
+        {
+            if (mini > board[i][result[i]])
+            {
                 mini = board[i][result[i]];
                 indexMin = result[i];
             }
-            board[i][result[i]] = 99;
-       //     std::cout << board[i][j] << "\t";
+            board[i][result[i]] = 1000;
         }
-     //   std::cout << "Minimalna w tym wierszu: " << mini << " ";
-     //   std::cout << "Minimalny index w tym wierszu: " << indexMin;
         indexes.push_back(indexMin);
         sum += mini;
         mini = INT_MAX;
-      //  std::cout << std::endl;
     }
-  //  std::cout << "Minimalna suma na tej tablicy to: " << sum << std::endl;
     map[sum] = indexes;
     indexes.clear();
 }
 
-
-void clearBoard(BoardContainter &bc) {
-    for (int i = 0; i < bc.board.size(); i++) {
-        for (int j = 0; j < bc.board[i].size(); j++) {
+void clearBoard(BoardContainter &bc)
+{
+    for (int i = 0; i < bc.board.size(); i++)
+    {
+        for (int j = 0; j < bc.board[i].size(); j++)
+        {
             bc.board[i][j] = bc.originalBoard[i][j];
         }
     }
 }
 
 void printBoard(BoardContainter &bc,
-                std::set<std::vector<int>> &solutions, Map &map) {
+                std::set<std::vector<int>> &solutions, Map &map)
+{
     int i = 1;
-    for (const auto &result: solutions) {
-     //   std::cout << "Board nr. " << i << ":" << std::endl;
+    for (const auto &result : solutions)
+    {
         printBoard(bc.board, result, map);
         clearBoard(bc);
-    //    std::cout << std::endl;
         i++;
     }
 }
 
-
-void fillBoard(std::vector<std::vector<int>> &board) {
-    for (auto &line: board) {
-        for (auto &value: line) {
+void fillBoard(std::vector<std::vector<int>> &board)
+{
+    for (auto &line : board)
+    {
+        for (auto &value : line)
+        {
             std::cin >> value;
         }
     }
 }
 
-void copyBoard(BoardContainter &bc) {
+void copyBoard(BoardContainter &bc)
+{
     bc.originalBoard.assign(bc.board.begin(), bc.board.end());
 }
 
-// void printMap(Map &map) {
-//     std::cout << std::endl;
-//     for (const auto &[minSum, indexesMin]: map) {
-//         std::cout << minSum << ": ";
-//         for(const int index : indexesMin){
-//             std::cout << index << " ";
-//         }
-//         std::cout << std::endl;
-//     }
-// }
-
-void printMinVector(Map &map) {
-   // std::cout << "\nIndeksy minimalnej sumy\n";
-    auto vec = std::min_element(map.begin(), map.end(), [](const auto &a, const auto &b) { return a < b; })->second;
-    for (const int &elem: vec) {
+void printMinVector(Map &map)
+{
+    auto vec = std::min_element(map.begin(), map.end(), [](const auto &a, const auto &b)
+                                { return a < b; })
+                   ->second;
+    for (const int &elem : vec)
+    {
         std::cout << elem << " ";
     }
 }
 
-int main() {
+int main()
+{
     int n;
     std::cin >> n;
 
-    if (n < 4) {
+    if (n < 4)
+    {
         std::cout << "Impossible to create\n";
         return 0;
     }
@@ -145,10 +150,9 @@ int main() {
     std::set<std::vector<int>> solutions;
 
     nHetmans(result, 0, n, solutions, board);
-    
+
     printBoard(bc, solutions, map);
 
-  //  printMap(map);
     printMinVector(map);
 
     return 0;
