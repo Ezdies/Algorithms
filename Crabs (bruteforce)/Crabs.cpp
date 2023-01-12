61190dae7480ca6ea31a52bdeeccba333750695c
Ezdies <bvbclubbvb@gmail.com>
2023-01-13 00:14:04 +0100

    Some improvements

--- Crabs (bruteforce)/Crabs.cpp
+++ Crabs (bruteforce)/Crabs.cpp
@@ -4,57 +4,68 @@ using namespace std;
 using vec1d = vector<int>;
 using vec2d = vector<vector<int>>;
 
+struct Vec1d
+{
+    vec1d occupyX;
+    vec1d occupyY;
+    vec1d occupyDiagM;
+    vec1d occupyDiagP;
+    vec1d res;
+
+    Vec1d(int n) : occupyX(n, 0), occupyY(n, 0), occupyDiagM(n * 2 + 1, 0), occupyDiagP(n * 2 + 1, 0), res(n, 0) {}
+};
+
 vec2d cost;
 int costRes = numeric_limits<int>::max();
-vec1d res;
 
-void het(int row, int n, vec1d &occupyX, vec1d &occupyDiagM, vec1d &occupyDiagP,
-         vec1d &occupyY, int _cost) {
-    if (row == n) {
-        if (_cost < costRes) {
+void het(int row, int n, int _cost, Vec1d &v1d)
+{
+    if (row == n)
+    {
+        if (_cost < costRes)
+        {
             costRes = _cost;
-            res = occupyY;
+            v1d.res = v1d.occupyY;
         }
         return;
     }
-    if (_cost >= costRes) return;
+    if (_cost >= costRes)
+        return;
 
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i)
+    {
         const int diagM = row - i + (n - 1);
         const int diagP = row + i;
-        if (occupyX[i] || occupyDiagM[diagM] || occupyDiagP[diagP]) continue;
-        occupyX[i] = occupyDiagM[diagM] = occupyDiagP[diagP] = true;
-        occupyY[row] = i;
+        if (v1d.occupyX[i] || v1d.occupyDiagM[diagM] || v1d.occupyDiagP[diagP])
+            continue;
+        v1d.occupyX[i] = v1d.occupyDiagM[diagM] = v1d.occupyDiagP[diagP] = true;
+        v1d.occupyY[row] = i;
         _cost += cost[row][i];
 
-        het(row + 1, n, occupyX, occupyDiagM, occupyDiagP, occupyY, _cost);
+        het(row + 1, n, _cost, v1d);
 
-        occupyX[i] = occupyDiagM[diagM] = occupyDiagP[diagP] = false;
+        v1d.occupyX[i] = v1d.occupyDiagM[diagM] = v1d.occupyDiagP[diagP] = false;
         _cost -= cost[row][i];
     }
 }
 
-void het(int n) {
-    vec1d occupyX(n, 0);
-    vec1d occupyY(n, 0);
-    vec1d occupyDiagM(n * 2 + 1, 0);
-    vec1d occupyDiagP(n * 2 + 1, 0);
-    het(0, n, occupyX, occupyDiagM, occupyDiagP, occupyY, 0);
-}
-
-int main() {
-    int n = 20;
+int main()
+{
+    int n;
     cin >> n;
+    Vec1d v1d(n);
     cost = vec2d(n, vector<int>(n));
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
             cin >> cost[i][j];
-            // cost[i][j] = 5;
         }
     }
-    het(n);
-    for (int i = 0; i < (int)res.size(); ++i) {
-        cout << res[i] << " ";
+    het(0, n, 0, v1d);
+    for (int i = 0; i < (int)v1d.res.size(); ++i)
+    {
+        cout << v1d.res[i] << " ";
     }
     cout << endl;
 }
