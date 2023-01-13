8ce37c92a8d7e2bcbf80e7d96a00a80efdbee110
Ezdies <bvbclubbvb@gmail.com>
2023-01-13 19:12:19 +0100

    Version without trap counting

--- RankHunt/RankHunt.cpp
+++ RankHunt/RankHunt.cpp
@@ -15,18 +15,28 @@
 // trick jest taki żeby używać stringów zamiast list
 
 // można zrobić to inaczej na jednowymiarowej tablicy
-//jak są dwie wartości na tym samym indexie, to dodać do stringa np
+// jak są dwie wartości na tym samym indexie, to dodać do stringa np
 
 #include <iostream>
 #include <string>
 
-void solve(int *crabs, int *crabsTotal, int n)
+void solve(int *crabs, int *crabsTotal, std::string *names, std::string *namesTotal, int n)
 {
     const int size = 100;
 
     for (int i = 0; i < n; i++)
     {
         crabsTotal[crabs[i]] = crabs[i];
+
+        //jeśli jest już zajęte, to dodaj do tego słowa
+        if (namesTotal[crabs[i]] != std::string())
+        {
+            namesTotal[crabs[i]].append(" " + names[i]);
+        }
+        else
+        {
+            namesTotal[crabs[i]] = names[i];
+        }
     }
 }
 
@@ -36,9 +46,22 @@ void printCrabsTotal(int *crabsTotal, const int N = 100)
     {
         if (crabsTotal[i] != 0)
         {
-            std::cout << crabsTotal[i] << '\n';
+            std::cout << crabsTotal[i] << ' ';
         }
     }
+    std::cout << '\n';
+}
+
+void printNamesTotal(std::string *namesTotal, const int N = 100)
+{
+    for (int i = N - 1; i >= 0; i--)
+    {
+        if (namesTotal[i] != std::string())
+        {
+            std::cout << namesTotal[i] << ' ';
+        }
+    }
+    std::cout << '\n';
 }
 
 void getInput(std::string *names, int *crabs, int *traps, int n)
@@ -76,13 +99,16 @@ int main()
 
     int *crabs = new int[n];
     int *traps = new int[n];
+
     int crabsTotal[totalCrabsNumber]{};
     int trapsTotal[totalTrapsNumber]{};
+    std::string namesTotal[totalCrabsNumber]{};
     int possibilitiesTotal[totalCrabsNumber * totalTrapsNumber];
 
     getInput(names, crabs, traps, n);
-    solve(crabs, crabsTotal, n);
+    solve(crabs, crabsTotal, names, namesTotal, n);
     printCrabsTotal(crabsTotal);
+    printNamesTotal(namesTotal);
     //  printInput(names, crabs, traps, n);
     freeMemory(names, crabs, traps);
 }
